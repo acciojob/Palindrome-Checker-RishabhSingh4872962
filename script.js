f30b3b1dd3eb413e68e8f89506e2806f1d882d3e
RishabhSingh4872962 <109566428+RishabhSingh4872962@users.noreply.github.com>
2024-03-09 20:08:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 // complete the given function
 
 function palindrome(str){
-let checkStr=str;
-	let strArr=str.split("").reverse().join("")
-	
-	return checkStr==strArr
+	let i=0;
+	let j=str.length-1;
+	while (i<j) {
+		if (str.indexOf(i) != str.indexOf(j)) {
+			return false;
+		}
+		i++;
+		j--;
+	}
+	return true;
 }
 module.exports = palindrome
